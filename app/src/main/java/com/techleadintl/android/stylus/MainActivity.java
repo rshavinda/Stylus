664b5afafdac06e1f5142461c0b5f1cecea42bca
Rajitha <rajitha@techleadintl.com>
2021-11-25 16:45:10 +0530

    Main UI design

--- app/src/main/java/com/techleadintl/android/stylus/MainActivity.java
+++ app/src/main/java/com/techleadintl/android/stylus/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
+    private static final String TAG = "MainActivity 1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
