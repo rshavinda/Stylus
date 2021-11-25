8ea781ddf0828eff27d68b66f7a6778d4f3eb43a
Rajitha <rajitha@techleadintl.com>
2021-11-25 11:58:30 +0530

    Initial commit

---
+++ app/src/main/java/com/techleadintl/android/stylus/MainHome.java
@@ -0,0 +1,14 @@
+package com.techleadintl.android.stylus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
