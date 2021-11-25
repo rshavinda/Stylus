c57e442af445a9fcf5dd98867b7c06f7efa7f269
Rajitha <rajitha@techleadintl.com>
2021-11-25 17:02:50 +0530

    code changes

--- app/src/main/java/com/techleadintl/android/stylus/MainActivity.java
+++ app/src/main/java/com/techleadintl/android/stylus/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity 1";
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
